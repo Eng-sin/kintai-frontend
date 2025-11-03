0a18e4894de8a12aeed9bec9168503b22fddb64b
Eng-sin <111264731+Eng-sin@users.noreply.github.com>
2025-11-03 18:04:36 +0900

    フロントからapiを呼び出せるよう簡易実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,160 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import { useEffect} from "react";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [message,setMessage] = useState("");
+  const [status,setStatus] = useState<any>(null);
+
+  const handleCheckIn = async () =>{
+    setMessage("");
+
+    try{
+      const res = await fetch("http://localhost:8080/api/attendance/check-in",{
+      method: "POST",
+      headers: {"Content-Type" : "application/json"},
+      body: JSON.stringify({userId:1}),
+      })
+
+      if(!res.ok){
+        const err = await res.json();
+        throw new Error(err.message || "エラーが発生しました");
+      }
+      await getStatus();
+      setMessage("勤務を開始しました")
+    } catch(err: any){
+      setMessage(`失敗しました： ${err.message}`);
+    }  
+  };
+
+  const handleCheckOut = async () =>{
+    setMessage("");
+    try{
+      const res = await fetch("http://localhost:8080/api/attendance/check-out",{
+      method: "POST",
+      headers: {"Content-Type" : "application/json"},
+      body: JSON.stringify({userId:1}),
+      });
+
+      if(!res.ok){
+        const err = await res.json();
+        throw new Error(err.message || "エラーが発生しました");
+      }
+      await getStatus();
+      setMessage("勤務を終了しました");
+
+    } catch(err: any){
+      setMessage(`失敗しました： ${err.message}`);
+    }
+  };
+
+    const breakStartTime = async () =>{
+    setMessage("");
+
+    try{
+      const res = await fetch("http://localhost:8080/api/attendance/break/start",{
+      method: "POST",
+      headers: {"Content-Type" : "application/json"},
+      body: JSON.stringify({userId:1}),
+      })
+
+      if(!res.ok){
+        const err = await res.json();
+        throw new Error(err.message || "エラーが発生しました");
+      }
+      await getStatus();
+      setMessage("休憩を開始しました")
+    } catch(err: any){
+      setMessage(`失敗しました： ${err.message}`);
+    }  
+  };
+
+  const breakEndTime = async () =>{
+    setMessage("");
+
+    try{
+      const res = await fetch("http://localhost:8080/api/attendance/break/end",{
+      method: "POST",
+      headers: {"Content-Type" : "application/json"},
+      body: JSON.stringify({userId:1}),
+      })
+
+      if(!res.ok){
+        const err = await res.json();
+        throw new Error(err.message || "エラーが発生しました");
+      }
+      await getStatus();
+      setMessage("休憩を終了しました")
+    } catch(err: any){
+      setMessage(`失敗しました： ${err.message}`);
+    }  
+  };
+
+  const getStatus = async () =>{
+      setMessage("");
+      try{
+        const res = await fetch("http://localhost:8080/api/attendance/status",{
+          method: "POST",
+          headers: {"Content-Type" : "application/json"},
+          body: JSON.stringify({userId:1}),
+        });
+        if(!res.ok){
+          const err = await res.json();
+          throw new Error(err.message || "エラーが発生しました");
+        }
+        const data = await res.json();
+        setStatus(data);
+        setMessage("勤務情報を取得しました");
+
+      } catch(err: any){
+        setMessage(`勤務情報の取得に失敗しました: ${err.message}`);
+      }
+  };
+
+  const STATUS_LABEL : Record<string,string> = {
+    "NOT_WORKING" : "未出勤",
+    "WORKING" : "勤務中",
+    "LEFT" : "勤務終了",
+    "BREAK" : "休憩中",
+    "ABSENT" : "欠勤",
+  };
+  
+  useEffect(() => {
+    getStatus();
+  },[]);
+  return (
+    <div>
+      {message && (
+        <div>{message}</div>
+      )}
+      <div>
+        <button onClick={handleCheckIn}>勤務開始</button>
+      </div>
+      <div>
+        <button onClick={breakStartTime}>休憩開始</button>
+      </div>
+      <div>
+        <button onClick={breakEndTime}>休憩終了</button>
+      </div>  
+      <div>
+        <button onClick={handleCheckOut}>勤務終了</button>
+      </div>
+      <div>
+        <button onClick={getStatus}>勤務情報取得</button>
+      </div>
+      {status && (
+        <div>
+          <ul>
+            <li>ステータス：{STATUS_LABEL[status.status] || "-"}</li>
+            <li>勤務開始時間：{status.checkInTime || "-"}</li>
+            <li>勤務終了時間：{status.checkOutTime || "-"}</li>
+            <li>休憩開始時間：{status.breakStartTime || "-"}</li>
+            <li>休憩終了時間：{status.breakEndTime || "-"}</li>
+          </ul>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      )}
     </div>
   );
 }
